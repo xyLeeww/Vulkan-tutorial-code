3b246f05a220c6114235f60d9eded81a9ccb0a1d
xinyli_amdeng <Xinyi.Li@amd.com>
2026-03-18 16:52:12 +0800

    Initial commit: HelloTriangle

---
+++ shaders/compile.bat
@@ -0,0 +1,2 @@
+C:\VulkanSDK\1.4.335.0\Bin\glslangValidator.exe -V D:\Code\HelloTriangle\shaders\shader.vert
+C:\VulkanSDK\1.4.335.0\Bin\glslangValidator.exe -V D:\Code\HelloTriangle\shaders\shader.frag
